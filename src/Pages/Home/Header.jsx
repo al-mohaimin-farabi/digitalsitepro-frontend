71409e9c8b5e967c8db8e089e6f1b796f9c4a729
Al Mohaimin Farabi <almohaiminfarabi.work@gmail.com>
2024-03-12 04:51:24 +0600

    responsive

--- src/Pages/Home/Header.jsx
+++ src/Pages/Home/Header.jsx
@@ -33,11 +33,11 @@ const Header = () => {
       ref={ref}
       className={`${HomeCss.header_bg} relative w-full py-16 px-0 overflow-hidden  h-[110vh] grid place-items-center `}>
       <div className="min-w-[100%] md:min-w-0 md:md:max-w-[960px] lg:max-w-[1280px]  py-12 grid place-items-center  text-center font-cardo text-balance z-[50]">
-        <h1 className=" relative text-white md:-mt-[30px] lg:-mt-[50px]  md:mb-[10px] lg:mb-[20px] text-[32px] sm:text-[40px] md:text-[45px] font-extrabold my-2 px-1  uppercase tracking-widest text-pretty break-words ">
+        <h1 className=" relative text-white md:-mt-[10px] lg:-mt-[50px]  md:mb-[10px] lg:mb-[20px] text-[32px] sm:text-[40px] md:text-[45px] font-extrabold my-2 px-1  uppercase tracking-widest text-pretty break-words ">
           Digital Site Pro <br />
           Studio <span className={`${HomeCss.gradient_text}`}>Live</span>
         </h1>
-        <p className="px-1 my-4 mb:my-2 lg:my-4 capitalize max-w-[380px] sm:max-w-[750px]  text-sm sm:text-base md:text-xl text-white font-normal text-balacne">
+        <p className="px-1 my-4 md:my-2 lg:my-4 capitalize max-w-[380px] sm:max-w-[750px]  text-sm sm:text-base md:text-xl text-white font-normal text-balacne">
           Experience the joy of collaborative creativity with us, where every
           moment sparks magic for our beloved customers.
         </p>
